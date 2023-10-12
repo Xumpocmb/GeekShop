2ac4e01551c1fcbc7a7d46a61694d06a781f6cb1
Xumpocmb <x.xumpocmb@gmail.com>
2023-10-12 20:05:00 +0300

    login page and register page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     user = authenticate(request, username=username, password=password)
+    #     if user is not None:
+    #         login(request, user)
+    #         return render(request, 'users/login.html')
+    return render(request, 'users/login.html')
+
+
+def registration(request):
+    return render(request, 'users/register.html')

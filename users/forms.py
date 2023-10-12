59bc80017bbf3bc90f868f1555e917c5572e909e
Xumpocmb <x.xumpocmb@gmail.com>
2023-10-12 21:16:51 +0300

    login & register done

--- users/forms.py
+++ users/forms.py
@@ -1,1 +1,36 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from users.models import User
+from django import forms
 
+
+class UserLoginForm(AuthenticationForm):
+    # переопределяем поля для указания стилей
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Имя пользователя'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Пароль'}))
+
+    # Связываем с моделью, чтобы django понимал, с какой моделью будем работать
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class UserRegistrationForm(UserCreationForm):
+    # переопределяем поля для указания стилей
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Имя'}))
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Фамилия'}))
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Имя пользователя'}))
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'placeholder': 'Email'}))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Пароль'}))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Повторите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
--- users/views.py
+++ users/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    # if request.method == 'POST':
-    #     username = request.POST.get('username')
-    #     password = request.POST.get('password')
-    #     user = authenticate(request, username=username, password=password)
-    #     if user is not None:
-    #         login(request, user)
-    #         return render(request, 'users/login.html')
-    return render(request, 'users/login.html')
+    context = {
+        'title': 'Авторизация в Geek Shop',
+        'form': UserLoginForm(),
+    }
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = auth.authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                context['error'] = 'Неверный логин или пароль'
+    return render(request, 'users/login.html', context=context)
 
 
 def registration(request):
-    return render(request, 'users/register.html')
+    context = {
+        'title': 'Регистрация в Geek Shop',
+        'form': UserRegistrationForm(),
+    }
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print('NOT VALID')
+    return render(request, 'users/register.html', context=context)

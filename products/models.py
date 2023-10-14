2d01d27e808b7ec79586ef15769cdc336c58a603
Xumpocmb <x.xumpocmb@gmail.com>
2023-10-14 10:09:19 +0300

    basket start

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 
 class ProductCategory(models.Model):
@@ -27,3 +28,13 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.category.name} | Продукт: {self.name}'
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина: {self.user.email} | Продукт: {self.product.name}'
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from users.models import User
@@ -8,10 +8,10 @@ from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 def login(request):
     context = {
         'title': 'Авторизация в Geek Shop',
-        'form': UserLoginForm(),
     }
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
+        print(form.errors)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
@@ -19,23 +19,25 @@ def login(request):
             if user is not None:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
-            else:
-                context['error'] = 'Неверный логин или пароль'
+    else:
+        form = UserLoginForm()
+    context['form'] = form
     return render(request, 'users/login.html', context=context)
 
 
 def registration(request):
     context = {
         'title': 'Регистрация в Geek Shop',
-        'form': UserRegistrationForm(),
     }
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Регистрация прошла успешно')
             return HttpResponseRedirect(reverse('users:login'))
-        else:
-            print('NOT VALID')
+    else:
+        form = UserRegistrationForm()
+    context['form'] = form
     return render(request, 'users/register.html', context=context)
 
 
@@ -48,7 +50,13 @@ def profile(request):
         form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Профиль обновлен')
             return HttpResponseRedirect(reverse('users:profile'))
         else:
             print(form.errors)
     return render(request, 'users/profile.html', context=context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))

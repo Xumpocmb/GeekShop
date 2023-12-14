6cb9c7acebe81a845cf765bdc7bfae0a11d4b7f9
Xumpocmb <x.xumpocmb@gmail.com>
2023-12-14 11:17:36 +0300

    CBV

--- products/views.py
+++ products/views.py
@@ -1,28 +1,34 @@
-from django.core.paginator import Paginator
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
 from products.models import Product, ProductCategory, Basket
+from common.views import TitleMixin
 
 
-def index(request):
-    context = {
-        'title': 'Geek Shop',
-        'username': 'Geek',
-    }
-    return render(request, 'products/index.html', context=context)
+class IndexView(TitleMixin, TemplateView):
+    template_name = 'products/index.html'
+    title = 'Geek Shop - Главная'
 
 
-def products(request, category_id=None, page=1):
-    products = Product.objects.filter(category__id=category_id) if category_id else Product.objects.all()
-    paginator = Paginator(products, 3)
-    products_paginator = paginator.page(page)
-    context = {
-        'title': 'Geek Shop',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-    }
-    return render(request, 'products/products.html', context=context)
+class ProductsListView(TitleMixin, ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+    title = 'Geek Shop'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            queryset = queryset.filter(category__id=category_id)
+        return queryset
 
 
 @login_required
--- users/views.py
+++ users/views.py
@@ -1,68 +1,44 @@
-from django.contrib import auth, messages
-from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+
+from common.views import TitleMixin
 from products.models import Basket
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from users.models import User
 
 
-def login(request):
-    context = {
-        'title': 'Авторизация в Geek Shop',
-    }
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        print(form.errors)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = auth.authenticate(request, username=username, password=password)
-            if user is not None:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context['form'] = form
-    return render(request, 'users/login.html', context=context)
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
 
 
-def registration(request):
-    context = {
-        'title': 'Регистрация в Geek Shop',
-    }
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Регистрация прошла успешно')
-            return HttpResponseRedirect(reverse('users:login'))
-    else:
-        form = UserRegistrationForm()
-    context['form'] = form
-    return render(request, 'users/register.html', context=context)
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
+    model = User
+    template_name = 'users/register.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('users:login')
+    success_message = "Вы успешно зарегистрировались!"
+    title = 'GeekShop - Регистрация'
 
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Профиль обновлен')
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            messages.error(request, 'Профиль не обновлен')
-            return HttpResponseRedirect(reverse('users:profile'))
-    else:
-        form = UserProfileForm(instance=request.user)
+class UserProfileView(TitleMixin, UpdateView):
+    model = User
+    template_name = 'users/profile.html'
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+    title = 'Geek Shop - Профиль'
 
+    def get_success_url(self):
+        return reverse('users:profile', args=[self.object.id])
 
-    context = {
-        'title': 'Профиль',
-        'form': form,
-        'basket': Basket.objects.filter(user=request.user),
-    }
-    return render(request, 'users/profile.html', context=context)
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data(**kwargs)
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
 
 
 def logout(request):
